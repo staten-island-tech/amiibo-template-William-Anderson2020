3f297e2c4eb169240c3a3266d459e4f1fc69c51e
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-12-17 11:46:17 -0500

    Backend progress
    Referecncing hbs templates

--- public/index.js
+++ public/index.js
@@ -2,8 +2,21 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.get('', (req, res) => {
-    res.send('Thami like bagels with butter.')
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+console.log(viewsPath);
+//setup handlebars engine and views location
+app.set('views engine', 'hbs'); //telling express/node to use handlebars for templates
+app.set('views', viewsPath); //telling node where to find the views
+
+app.get('', async (req, res) => {
+    //res.send('Thami like bagels with butter... Or does he?')
+    try {
+        res.render('index');
+    } catch (err) {
+        res.status(500).send();
+    }
 });
 
 app.get('/melkadze', (req, res) => {
@@ -11,5 +24,5 @@ app.get('/melkadze', (req, res) => {
 });
 
 app.listen(3000, (req, res) => {
-    console.log('Port 3000 speaks');
+    console.log('Port 3000 speaks.');
 });
