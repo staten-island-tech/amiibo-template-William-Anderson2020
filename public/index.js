e0263396d3c78178a581feab2dfb620a065b6c49
William-Anderson2020 <48596907+William-Anderson2020@users.noreply.github.com>
2019-12-19 11:01:46 -0500

    Catching up on day 2
    I've returned to the land of the living

--- public/index.js
+++ public/index.js
@@ -1,29 +1,48 @@
-const path = require('path');
-const express = require('express');
-const view = require('view');
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
 const app = express();
 
 //define paths for express
-const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates/views');
-console.log(viewsPath);
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials"); //sets up variable directory for hbs
+const viewsPath = path.join(__dirname, "../templates/views"); //where to find hbs files
 //setup handlebars engine and views location
-app.set('views engine', 'hbs'); //telling express/node to use handlebars for templates
-app.set('view', viewsPath); //telling node where to find the views
-
-app.get('', async (req, res) => {
-    //res.send('Thami like bagels with butter... Or does he?')
-    try {
-        res.render('index');
-    } catch (err) {
-        res.status(500).send();
-    }
+app.set("view engine", "hbs"); //telling express/node to use handlebars for templates
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
+hbs.registerPartials(partialsPath); //telling program where to ref. partials from
+app.get("", async (req, res) => {
+  try {
+    res.render("index", {
+      title: "Our First Express App" //sets hbs var value?
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+app.get("/about/:id", async (req, res) => {
+  const name = req.params.id; //?
+  try {
+    res.render("index", { //load index.hbs
+      title: name //set title to name for index.hbs
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+app.get("/anything", async (req, res) => { //display for /anything extension
+  try {
+    res.render("swag"); //load swag.hbs
+  } catch {
+    res.status(500).send();
+  }
 });
 
-app.get('/melkadze', (req, res) => {
-    res.send('Choco milk')
+app.listen(3000, () => {
+  console.log("Port 3000 speaks.");
+  console.log(viewsPath);
 });
 
-app.listen(3000, (req, res) => {
-    console.log('Port 3000 speaks.');
-});
+//127.0.0.1:3000
+//or
+//localhost:3000
